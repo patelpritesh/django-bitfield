ff04f10825c2ddcce1744caabc5e45ff724ff13a
Pritesh Patel <pritesh@trendlyne.com>
2021-08-05 20:20:02 +0530

    django 3 doesnot support context

--- bitfield/models.py
+++ bitfield/models.py
@@ -155,7 +155,7 @@ class BitField(BigIntegerField):
     #         return BitQuerySaveWrapper(self.model._meta.db_table, self.name, value)
     #     return super(BitField, self).get_db_prep_save(value, connection=connection)
 
-    def from_db_value(self, value, expression, connection, context):
+    def from_db_value(self, value, expression, connection, context=None):
         return self.to_python(value)
 
     def get_db_prep_lookup(self, lookup_type, value, connection, prepared=False):
